1820671aece43bcba5bc324b6b30c241f7dd6905
Georges David <111468723+GeorgessDavid@users.noreply.github.com>
2026-05-29 15:14:44 -0300

    Frontend: Initial commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Ubuntu, Inter } from "next/font/google";
+import { Header } from '@/components';
+import "./globals.css";
+
+const ubuntu = Ubuntu({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-ubuntu',
+});
+
+const inter = Inter({
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter'
+})
+
+
+
+export const metadata: Metadata = {
+  title: "CrunchiStore",
+  description: "Encontrá al Otaku que hay en vos.",
+  icons: "/logo.png"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="es"
+      className={`${ubuntu.variable} ${inter.variable} bg-background h-full antialiased`}
+      data-scroll-behavior="smooth"
+    >
+      <body className={`${ubuntu.className} ${inter.className}`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
