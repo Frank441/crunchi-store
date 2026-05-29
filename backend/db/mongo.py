03946b5a9c279892e1436fb438a55bf7a7a2d57d
Frank441 <stormform1234@gmail.com>
2026-05-28 23:47:11 -0300

    feat(back, core): base mongo file connection

---
+++ backend/db/mongo.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from pymongo.database import Database
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB  = os.getenv("MONGO_DB")
+
+client = MongoClient(MONGO_URI)
+
+db: Database = client[MONGO_DB]
